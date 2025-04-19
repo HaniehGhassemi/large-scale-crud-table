b15232ce535fb64cc4dfbad715d8b2ec8d801df0
Hanieh Ghassemi <haniehghsmie@gmail.com>
2025-04-19 08:24:21 +0330

    feat(LST-18): virtual scroll

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,14 +1,17 @@
 import { RootState } from '@/stores/store';
 import { toggleTheme } from '@/stores/themeSlice';
-import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import IconButton from '../IconButton/IconButton';
 import { DarkIcon, LightIcon } from '@/assets/icons';
 import { useEffect } from 'react';
+import CONSTANTS from '@/shared/types/constants';
+import { AccentColors } from '@/shared/types/enums';
+import { useMediaQuery } from 'react-responsive';
 
-const ThemeToggle: React.FC = () => {
+const ThemeToggle = () => {
   const dispatch = useDispatch();
   const isDarkMode = useSelector((state: RootState) => state.theme.isDarkMode);
+  const isLargeScreen = useMediaQuery({ minWidth: CONSTANTS.BREAKPOINTS.LG });
 
   const handleToggle = () => {
     dispatch(toggleTheme());
@@ -27,6 +30,7 @@ const ThemeToggle: React.FC = () => {
       icon={isDarkMode ? <DarkIcon /> : <LightIcon />}
       onClick={handleToggle}
       title={isDarkMode ? 'switch to dark mode' : 'switch to light mode'}
+      color={isLargeScreen ? AccentColors.Primary : AccentColors.Secondary}
     />
   );
 };
